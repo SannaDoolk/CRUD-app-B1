0768869cda6703dc3f13612b98bc5e9d89ceec39
Sanna Doolk <sd222si@student.lnu.se>
2021-05-04 10:57:41 +0200

    Started configurate server.js

---
+++ src/server.js
@@ -0,0 +1,38 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Sanna Doolk
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import hbs from 'express-hbs'
+// import session from 'express-session'
+// import helmet from 'helmet'
+import logger from 'morgan'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+// Create express application
+const app = express()
+
+// Get the directory name of this module's path.
+const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
+app.use(logger('dev'))
+
+app.engine('hbs', hbs.express4({
+  defaultLayout: join(directoryFullName, 'views', 'layouts', 'default'),
+  partialsDir: join(directoryFullName, 'views', 'partials')
+}))
+app.set('view engine', 'hbs')
+app.set('views', join(directoryFullName), 'views')
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static(join(directoryFullName, '..', 'public')))
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`)
+  console.log('Press Ctrl-C to terminate...')
+})
