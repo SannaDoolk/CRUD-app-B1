c897e5b0ac7c6ca374dc3d607b769b85a83b39d9
Sanna Doolk <sd222si@student.lnu.se>
2021-05-05 16:44:04 +0200

    Added  methods to render more  pages in controller

--- src/controllers/crud-controller.js
+++ src/controllers/crud-controller.js
@@ -15,10 +15,12 @@ export class CrudController {
  * @param {object} req - Express request object.
  * @param {object} res - Express response object.
  */
+
+  // GLÖM EJ GÖRA ASYNC
   index (req, res) {
     res.render('crud/index')
   }
-
+  
   logIn (req, res) {
     res.render('crud/log-in')
   }
@@ -29,5 +31,17 @@ export class CrudController {
 
   createNew (req, res) {
     res.render('crud/create')
+  }
+
+  read (req, res) {
+    res.render('crud/read')
+  }
+
+  update (req, res) {
+    res.render('crud/update')
+  }
+
+  delete (req, res) {
+    res.render('crud/delete')
   }
 }
